744c704f8289a8216c106083da2c0a43c1b82f79
saneechka <alexandro.dev.work@gmail.com>
2025-01-16 13:34:58 +0300

    clear useless

--- scripts/export_database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Создаем директорию для дампа если её нет
-mkdir -p scripts/init-data
-
-
-mysqldump -h localhost -u root -p currency_db > scripts/init-data/dump.sql
-
-echo "База данных экспортирована в scripts/init-data/dump.sql"
